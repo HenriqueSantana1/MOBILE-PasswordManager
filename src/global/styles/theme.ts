99feb04a1adf5f2c355088a56f769e4c8444d485
Henrique <henrique.santana@ecomp.ufsm.br>
2021-07-09 11:42:39 -0300

    project structure

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,13 @@
+import { Roboto_300Light, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+
+export const theme = {
+    colors: {
+      primary: '#55EFE0',
+    },
+  
+    fonts: {
+      roboto300: Roboto_300Light,
+      roboto400: Roboto_400Regular,
+      roboto700: Roboto_700Bold
+    }
+  };
---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+import { styles } from './styles';
+
+export function SignIn() {
+
+  return (
+    <View style={styles.container}>
+        <Text>SignIn</Text>
+    </View>
+  );
+}
